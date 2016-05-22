0580cc536734f82e4a18ea020ebd47101e7262ee
J.Raigneau <julien@zeneffy.fr>
2016-05-22 20:57:16 +0200

    added weather widget

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 
-domi-gigue
+	  ____ _
+	 / ___(_) __ _ _   _  ___
+	| |  _| |/ _` | | | |/ _ \
+	| |_| | | (_| | |_| |  __/
+	 \____|_|\__, |\__,_|\___|
+	         |___/
 
-using dashing beautiful framework, sinatra_cyclist and docker to display useful info for my home automation system on on (very) old ipad
+
+Using dashing beautiful framework, sinatra_cyclist and docker to display useful info for my home automation system on on (very) old ipad
 
 
--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -v=/home/superman/dev/domi-gigue/dashboards:/dashboards -v=/home/superman/dev/domi-gigue/config:/config -v=/home/superman/dev/domi-gigue/widgets:/widgets -v=/home/superman/dev/domi-gigue/assets:/assets -e GEMS="sinatra_cyclist" --name gigue -d -p 8080:3030 frvi/dashing
+docker run -v=/home/superman/dev/domi-gigue/dashboards:/dashboards -v=/home/superman/dev/domi-gigue/config:/config -v=/home/superman/dev/domi-gigue/widgets:/widgets -v=/home/superman/dev/domi-gigue/assets:/assets -v=/home/superman/dev/domi-gigue/jobs:/jobs -e GEMS="sinatra_cyclist json" --name gigue -d -p 8080:3030 frvi/dashing
